483e826748f8ae436dc0a5e49d621ba281ca2370
Deemz <dima.baibus@gmail.com>
2021-10-27 16:43:08 +0200

    [MAT-9] Page Login add https

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const fs = require("fs");
+const https = require("https");
+
 dotenv.config();
 
 const app = express();
 global.__basedir = __dirname;
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: false,
+  //origin: "http://localhost:3000"
 };
 
 app.use(cors());
-
+/* app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); */
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 /* app.use((req, res, next) => {
@@ -36,10 +45,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./server/routes/user.routes")(app);
 require("./server/routes/materiele.routes")(app);
 
+const privateKey = fs.readFileSync("./.cert/key.pem", "utf8");
+const certificate = fs.readFileSync("./.cert/cert.pem", "utf8");
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+httpsServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import TokenService from '../src/services/token.service'
+import TokenService from '../src/services/token.service';
+import https from "https";
 
 /* export default axios.create({
   //baseURL: "http://localhost:3001/api",
@@ -11,16 +12,24 @@ import TokenService from '../src/services/token.service'
     //'Authorization': `Bearer ${localStorage.getItem('x-access-token')}`,
   }
 }); */
-
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // (NOTE: this will disable client verification)
+ 
+})
 const axiosApiInstance =  axios.create({
-  baseURL: "http://localhost:3001/api",
-  //baseURL: "http://192.168.176.35:3001/api",
+ 
+  //baseURL: "http://localhost:3001/api",
+  baseURL: "https://192.168.176.35:3001/api",
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE',
-    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    
     //'Authorization': `Bearer ${localStorage.getItem('x-access-token')}`,
-  }
+  },
+  //mode: 'no-cors',
+  //crossdomain: true
 });
 //export default axios.create();
 // Request interceptor for API calls
@@ -44,6 +53,7 @@ axiosApiInstance.interceptors.request.use(
 axiosApiInstance.interceptors.response.use(
   (res) => {
     console.log(res)
+ 
     return res;
   },
   async (err) => {
@@ -53,7 +63,6 @@ axiosApiInstance.interceptors.response.use(
       // Access Token was expired
       if (err.response.status === 401 && !originalConfig._retry) {
         originalConfig._retry = true;
-
         /* try {
           const rs = await instance.post("/auth/refreshtoken", {
             refreshToken: TokenService.getLocalRefreshToken(),
